793e310cd3dd5b40d1afd321da0e4ecb6d25b491
Sonuo7 <rockysingh12ops@gmail.com>
2024-11-27 23:13:45 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -21,7 +21,7 @@ async def start(bot, message):
         InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/alsamovies"),
         InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/alsamovies")
     ]]
-    await message.reply_photo(photo='link', 
+    await message.reply_photo(photo='https://envs.sh/3bL.jpg', 
 caption=script.START.format(message.from_user.mention),
                         disable_web_page_preview=True,
                         reply_markup=InlineKeyboardMarkup(button))
